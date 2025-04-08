15db745c125ec70f74a8c7b99754f2c944d0ffbc
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-08 15:25:47 +0530

    create mongodb connection succesfully

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ import express from "express";
 
 let app = express()
 
-app.use(cors());
 
-const mongoUrl = ""
+
+const mongoUrl = "mongodb+srv://abc:root@cluster0.i2xn0i4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoUrl)
+let connection = mongoose.connection
+
+connection.once("open",()=>{
+    console.log("MongoDB connection is successfully")
+})
+
+app.listen(4000,()=>{
+    console.log("server is running on port 4000")
+})
 
 
 
