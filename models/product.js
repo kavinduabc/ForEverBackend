0b83a1d78fe79fc6ab7c3af24af47a74d770c134
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-12 20:45:49 +0530

    done product model

---
+++ models/product.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+
+    id:{
+        type : Number,
+        required : true,
+        unique : true
+    },
+    name :{
+       type : String,
+       required :true 
+    },
+    price : {
+        type : String,
+        required :true
+    },
+    category :{
+        type :String,
+        required : true
+    },
+    dimention :{
+        type : String,
+        required : true
+    },
+    availability :{
+        type : Boolean,
+        default : true
+    },
+    image:{
+        type : [String],
+        required : true,
+        default :["https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fuser%2F&psig=AOvVaw0uCU9t72Sm51_RBTIJ2OxU&ust=1739456529253000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCbiuCqvosDFQAAAAAdAAAAABAE"]
+      }
+})
+
+const Product = mongoose.model("product",productSchema);
+
+export default Product;
