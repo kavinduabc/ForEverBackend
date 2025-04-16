fc6de6682eb09c2949d38bc9b57cbafd6c486919
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-16 20:28:48 +0530

    😢😢

--- route/productRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addProduct } from "../controller/productController.js";
-
-const productRouter = express.Router();
-
-productRouter.post("/add", addProduct);
-
-export default productRouter;
