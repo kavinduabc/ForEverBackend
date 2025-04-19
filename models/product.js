1a8c10ed1017a307796cff3d2e0d5e070eb30c4e
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-19 13:38:47 +0530

    modify the product model

--- models/product.js
+++ models/product.js
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
-
-    id:{
-        type : Number,
-        required : true,
-        unique : true
+const productShema = mongoose.Schema({
+    image:{
+        type :[string],
+        required:true,
+        default :["https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fuser%2F&psig=AOvVaw0uCU9t72Sm51_RBTIJ2OxU&ust=1739456529253000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCbiuCqvosDFQAAAAAdAAAAABAE"]
     },
-    name :{
-       type : String,
-       required :true 
-    },
-    price : {
+    name : {
         type : String,
-        required :true
+        required : true
+    },
+    discription :{
+        type : String,
+        required : true
     },
     category :{
-        type :String,
-        required : true
-    },
-    dimention :{
         type : String,
         required : true
     },
-    availability :{
-        type : Boolean,
-        default : true
+    subCategory :{
+        type : String,
+        required : true
     },
-    image:{
+    price :{
+        type : Number,
+        required : true
+    },
+    size :{
         type : [String],
-        required : true,
-        default :["https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fuser%2F&psig=AOvVaw0uCU9t72Sm51_RBTIJ2OxU&ust=1739456529253000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCbiuCqvosDFQAAAAAdAAAAABAE"]
-      }
+        required : true
+    }
+
 })
 
-const Product = mongoose.model("product",productSchema);
+const Product = mongoose.model("product",productShema);
 
 export default Product;
