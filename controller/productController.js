aa0ea93adcf98bd94d88b22173fff02e37b85546
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-21 21:44:36 +0530

    fixd some errors

--- controller/productController.js
+++ controller/productController.js
@@ -1,37 +1,34 @@
-import Product from "../models/product.js"
+import Product from "../models/product.js";
 
-export async function addProduct(req,res) {
-    
-    console.log("Decoded user :",req.user);
+export async function addProduct(req, res) {
+  console.log("Decoded user:", req.user);
 
-    if(req.user == null)
-    {
-        res.status(401).jsoan({
-            message : "Please Loging and try again"
-        })
-        return 
-    }
-    if(req.user.role != "admin")
-    {
-        res.status(401).jsoan({
-            message :"you are not authirize preform this task"
-        })
-        return
-    }
+  if (req.user == null) {
+    res.status(401).json({
+      message: "Please login and try again",
+    });
+    return;
+  }
 
-    const data  = req.body;
-    const newProduct = new Product(data);
+  if (req.user.role !== "admin") {
+    res.status(403).json({
+      message: "You are not authorized to perform this task",
+    });
+    return;
+  }
 
-    try{
-        await newProduct.save();
-        res.json({
-            message :"Product registered successfull"
-        })
-    }
-    catch(error){
-        res.status(500).json({
-            error :"product registration faild"
-        })
-    }
+  const data = req.body;
+  const newProduct = new Product(data);
 
+  try {
+    await newProduct.save();
+    res.json({
+      message: "Product registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Product registration failed",
+    });
+  }
 }
