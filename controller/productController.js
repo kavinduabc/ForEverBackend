9ed0c6f7f2e64cb973009f35d8db239e620e024e
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-22 07:24:48 +0530

    implement the view product function

--- controller/productController.js
+++ controller/productController.js
@@ -11,7 +11,7 @@ export async function addProduct(req, res) {
   }
 
   if (req.user.role !== "admin") {
-    res.status(403).json({
+    res.status(401).json({
       message: "You are not authorized to perform this task",
     });
     return;
@@ -31,4 +31,33 @@ export async function addProduct(req, res) {
       error: "Product registration failed",
     });
   }
+}
+
+export async function  getProduct(req,res){
+    
+    console.log("Decoded user",req.user);
+
+    if(req.user == null){
+        res.status(401).json({
+            message:"please login and try again"
+        })
+    }
+    if(req.user.role != "admin"){
+        res.status(401).json({
+            message : "you are not autorized to perform this task"
+        })
+    }
+    else{
+        try {
+
+            const products =await Product.find()
+            res.json(products);
+            return ;
+            
+        } catch (error) {
+            res.status(500).json({
+                message : "faild to get product"
+            })
+        }
+    }
 }
--- route/productRouter.js
+++ route/productRouter.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { addProduct } from "../controller/productController.js";
+import { addProduct, getProduct } from "../controller/productController.js";
 
 const producRouter = express.Router();
 producRouter.post("/",addProduct);
-
+producRouter.get("/",getProduct)
 export default producRouter;
